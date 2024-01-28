198ce72fc70ded51b8b2e0ea8a495f4c04c33829
MugoDom <njuemugodominic@gmail.com>
2024-01-29 01:21:46 +0300

    Modifies the chatbot responses

--- deployment/streamlit_app.py
+++ deployment/streamlit_app.py
@@ -3,6 +3,8 @@ import joblib
 import random
 import time
 
+
+
 # Streamlit app title
 st.title("Saidika")
 
@@ -23,17 +25,23 @@ def predict_intent(user_input):
 
     return intent
 
-# Function to generate responses based on predicted intents
+
+#Function to generate responses based on predicted intents
+
 def generate_response(intent):
     # Implement your logic here to generate appropriate responses based on the predicted intents
     if intent == 'alcoholism':
-        response = "I am sorry for what you are going through. Dealing with alcoholism can be a big challenge for everyone. However, you can always get help to get out of the addiction. Do you mind sharing with me what kind of help you would want?"
+        response = "I am sorry for what you are going through. Dealing with alcoholism can be a big challenge for everyone. However, you can always get help to get out of the addiction. Stay strong. The road to recovery may be challenging, but it leads to a better, healthier life."
     elif intent == 'diagnosedPTSD':
-        response = "Goodbye! Take care."
+        response = "I'm really concerned about you. It's important to prioritize your safety. Can you please reach out to a trusted friend, family member, or a helpline? Your life is valuable, and there are people who want to help you through this."
     elif intent == 'socialanxiety':
-        response = "I'm sorry, I don't have the information you're looking for."
+        response = "I understand that social interactions can be challenging, and I want you to know that your feelings are valid. It's completely okay to take things at your own pace. If there's anything specific you'd like to talk about or if you have questions, feel free to share. I'm here to support you in a way that feels comfortable for you.."
+    elif intent == 'Suicidewatch':
+        response = "It might be helpful to reach out to a trusted friend, family member, or mental health professional for support."
+    elif intent == 'CPTSD':
+        response = "I'm truly sorry to hear that you're going through a tough time with complex post-traumatic stress disorder. It's totally okay to take things one step at a time. If you're comfortable sharing, Saidika is here for you."
     else:
-        response = "I'm here to help. Please let me know how I can assist you."
+        response = "I am sorry I don't have the information you are looking for."
 
     return response
 
